e84547a0b1af20af26c477553f70cbcabcc61512
bemays <may18019@byui.edu>
2022-06-29 14:56:24 -0600

    changes to work_page.py

--- pages/work_page.py
+++ pages/work_page.py
@@ -12,15 +12,20 @@ def get_data():
 df = get_data()
 
 # Write a header that says "Learning how to use Streamlit"
-
-
+st.write("""# Learning how to use Streamlit """)
 
 # Create a check box labeled "Show data" that when checked, displays the dataframe
-
-
+y = st.checkbox('Show data')
+if y:
+    st.write(df)
 
 # Create a slider that allows the user to choose a range between two years from our movie dataset
+min_year, max_year = st.slider('Year Range', value=(1893,2005), min_value = 1900, max_value = 2000)
 
+def get_data_sorted_by_year():
+    return df.sort_values("year",ascending=False) 
+df_sorted_by_year = get_data_sorted_by_year()
+st.write(df_sorted_by_year.head(min_year, max_year))
 # start_color, end_color = st.select_slider(
 #      'Select a range of color wavelength',
 #      options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'],
